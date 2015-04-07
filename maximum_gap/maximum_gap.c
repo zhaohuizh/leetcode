d71f6bfa9e7a60b017dbf7b0a7879d92c200b804
Zhaohui <zhaohuz@amazon.com>
2015-04-06 18:41:20 -0700

    maximum gap not finished yet

--- maximum_gap/maximum_gap.c
+++ maximum_gap/maximum_gap.c
@@ -1,1 +1,13 @@
-// For future commit
+#include <limits.h>
+
+int maximumGap(int num[], int n) {
+  int min = INT_MAX, max = 0;
+  int i;
+  for(i = 0; i < n; i++){
+    if(num[i] > max){
+      max = num[i];
+    }else if(num[i] < min){
+      min = num[i];
+    }
+  }
+}
