b90563dfa098a053e5589f0748739c7e75a2169e
Zhaohui <zhaohuz@amazon.com>
2015-04-07 12:10:50 -0700

    min stack signatures

---
+++ min_stack/min_stack.c
@@ -0,0 +1,27 @@
+typedef struct {
+
+} MinStack;
+
+void minStackCreate(MinStack *stack, int maxSize) {
+
+}
+
+void minStackPush(MinStack *stack, int element) {
+
+}
+
+void minStackPop(MinStack *stack) {
+
+}
+
+int minStackTop(MinStack *stack) {
+
+}
+
+int minStackGetMin(MinStack *stack) {
+
+}
+
+void minStackDestroy(MinStack *stack) {
+
+}
