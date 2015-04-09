250c6ddcb20fa2d15807f193525d91f222a4ab6a
zhaohuizh <bravezhaohui@gmail.com>
2015-04-08 21:10:54 -0700

    a greedy method

--- gas_stations/gas_stations.c
+++ gas_stations/gas_stations.c
@@ -1,2 +1,17 @@
 int canCompleteCircuit(int gas[], int cost[], int n) {
+  int idx = 0;
+  int total = 0, sum = 0;
+  for(int i = 0; i < n; i++){
+    int diff = gas[i] - cost[i];
+    total += diff;
+    sum += diff;
+    if(sum < 0){
+      idx = i + 1;
+      sum = 0;
+    }
+  }
+  if(total < 0){
+    return -1;
+  }
+  return idx;
 }
